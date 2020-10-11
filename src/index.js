9ac72b5a2bea856ddcb726b3c92440cfee5f94e1
Chaiwtun <chaiwtun@scg.com>
2020-10-11 17:48:33 +0700

    [master] test push

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const port = process.env.PORT || 3000;
 const server = http.createServer((_, res) => {
   res.setHeader('Content-Type', 'text/html');
-  res.write("Hello world from cloud build");
+  res.write("Hello world from cloud build V.1.1");
   res.end();
 });
 
